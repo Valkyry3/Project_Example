465920bd5ee1c9721304c83e3aec072f2262c3f1
Victoria Wilson <valkyry3@gmail.com>
2018-02-27 10:40:11 -0600

    bleh

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
+var visibleText = "Show Less";
+var hiddenText = "Show More";
+
 $(document).ready(function(){
-    $(".infoBox button").click(function(){
-        $(this).next().toggleClass("visible");
+    $("button").click(function(){
+        $(this).toggleClass("visible");
+       if$((this).hasClass) {
+          $(this).html(hiddenText);
+        } else {
+          $(this).html(visibleText);
+        } $(this).next().toggleClass("visible");
     });
 })
